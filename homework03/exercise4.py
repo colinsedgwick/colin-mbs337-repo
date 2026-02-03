9e4b4bfd63770958c4d3e85a59fcf5067eca8cf4
Colin Sedgwick <colinsedgwick@utexas.edu>
2026-02-03 08:50:16 +0000

    updated homework03 exercise4 with comments

--- homework03/exercise4.py
+++ homework03/exercise4.py
@@ -1,8 +1,11 @@
+# import json and yaml libraries
 import json
 import yaml
 
+# open json file and save dictionary to python variable
 with open('uniprot_data.json', 'r') as f:
     protein_data = json.load(f)
 
+# write dictionary to yaml file without sorting dictionary keys, and having an explicit start and end to yaml file
 with open('proteins.yaml', 'w') as o:
     yaml.dump(protein_data, o, sort_keys=False, explicit_start=True, explicit_end=True)
