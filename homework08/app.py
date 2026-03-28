f0b26a7a0be69a53c2ef5ad18d0b69cc04b77f4c
Colin Sedgwick <colinsedgwick@utexas.edu>
2026-03-28 21:59:46 +0000

    homework08 integration test attempt 5

--- homework08/app.py
+++ homework08/app.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash()
 server = app.server
 
-app.layout = [html.Div(children='Hello, World!')]
+app.layout = [html.Div(children='Hello, World! (test)')]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8050, debug=True)
